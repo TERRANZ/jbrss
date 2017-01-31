f60fa7aa78ae305bffa5e7a09b742dd6cd6763f8
Vadim.Korostelev <Vadim.Korostelev@thomsonreuters.com>
2017-01-31 15:12:19 +0300

    auth

---
+++ services/registration/src/main/java/ru/terra/jbrss/config/AuthorizationConfigurerAdapter.java
@@ -0,0 +1,20 @@
+package ru.terra.jbrss.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationConfigurerAdapter extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("acme")
+                .secret("acmesecret")
+                .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password", "client_credentials")
+                .scopes("read", "write");
+    }
+}
