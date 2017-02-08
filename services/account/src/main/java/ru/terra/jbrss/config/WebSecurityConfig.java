cc8752d6f71fac02f914c37d18c1f8400b1875a0
Vadim.Korostelev <Vadim.Korostelev@thomsonreuters.com>
2017-02-08 12:16:33 +0300

    incr

--- services/account/src/main/java/ru/terra/jbrss/config/WebSecurityConfig.java
+++ services/account/src/main/java/ru/terra/jbrss/config/WebSecurityConfig.java
@@ -5,10 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,20 +18,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .exceptionHandling()
-//                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/**").authenticated()
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/oauth/**").permitAll();
-////                .and()
-////                .httpBasic();
-//    }
 }
