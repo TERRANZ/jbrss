7e8b3927db21d719b4cee291fa325dd0effc3594
vadim.korostelev <Vadim.Korostelev@thomsonreuters.com>
2017-03-30 16:31:22 +0300

    indexes

--- services/rsscore/src/main/java/ru/terra/jbrss/db/entity/Feedposts.java
+++ services/rsscore/src/main/java/ru/terra/jbrss/db/entity/Feedposts.java
@@ -5,28 +5,37 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Table(name = "feedposts")
+@Table(name = "feedposts", indexes = {
+        @Index(columnList = "id", name = "fp_id_idx"),
+        @Index(columnList = "postdate", name = "fp_postdate_idx")
+})
 @NamedQuery(name = "Feedposts.getPostsByFeedAndByDateSortedUnread", query = "SELECT f FROM Feedposts f WHERE f.feedId = ?0 AND f.isRead = ?1 ORDER BY f.postdate DESC")
 public class Feedposts implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id", nullable = false)
     private Integer id;
+
     @Basic(optional = false)
     @Column(name = "feed_id", nullable = false)
     private int feedId;
+
     @Basic(optional = false)
     @Column(name = "postdate", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private Date postdate = new Date();
+
     @Basic(optional = false)
     @Column(name = "posttitle", nullable = false, length = 512)
     private String posttitle;
+
     @Basic(optional = false)
     @Column(name = "postlink", nullable = false, length = 512)
     private String postlink;
+
     @Basic(optional = false)
     @Lob
     @Column(name = "posttext", nullable = false, length = 2147483647)
