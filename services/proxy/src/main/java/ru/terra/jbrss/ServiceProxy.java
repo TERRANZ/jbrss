db782957774fd3d7cca3b33097e5748f050a7bdc
terranz <terranz@terranz.mine.nu>
2017-04-23 00:27:25 +0300

    zuul and eureka divided

--- services/proxy/src/main/java/ru/terra/jbrss/ServiceProxy.java
+++ services/proxy/src/main/java/ru/terra/jbrss/ServiceProxy.java
@@ -4,17 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Import;
-import ru.terra.jbrss.config.AccountsWebApplicationConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@SpringBootApplication
+@EnableZuulProxy
 @EnableAutoConfiguration
 @EnableEurekaClient
-@Import(AccountsWebApplicationConfiguration.class)
-@SpringBootApplication
-public class AccountApplication {
+public class ServiceProxy {
 
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "accounts-server");
-        SpringApplication.run(AccountApplication.class, args);
+        System.setProperty("spring.config.name", "proxy-server");
+        SpringApplication.run(ServiceProxy.class, args);
     }
 }
--- services/registration/src/main/java/ru/terra/jbrss/ServiceRegistrationServer.java
+++ services/registration/src/main/java/ru/terra/jbrss/ServiceRegistrationServer.java
@@ -2,12 +2,12 @@ package ru.terra.jbrss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableZuulProxy
+@EnableDiscoveryClient
 public class ServiceRegistrationServer {
 
     public static void main(String[] args) {
