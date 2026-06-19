7165a056aef0b0bb68d2e9d14bda77612afaebe2
Vadim Korostelev <terranz@terranz.mine.nu>
2026-06-19 14:01:11 +0400

    Add CRUD methods to DataManager

--- src/main/java/com/jbrss/DataManager.java
+++ src/main/java/com/jbrss/DataManager.java
@@ -10,5 +10,20 @@ public class DataManager {
         return rootFolder;
     }
 
-    // Methods for CRUD will go here later or in specific services
+    public void addFolder(Folder parent, String name) {
+        Folder newFolder = new Folder(name);
+        parent.getSubFolders().add(newFolder);
+    }
+
+    public void removeFolder(Folder parent, Folder folderToRemove) {
+        parent.getSubFolders().remove(folderToRemove);
+    }
+
+    public void addFeed(Folder folder, RSSFeed feed) {
+        folder.getFeeds().add(feed);
+    }
+
+    public void removeFeed(Folder folder, RSSFeed feed) {
+        folder.getFeeds().remove(feed);
+    }
 }
